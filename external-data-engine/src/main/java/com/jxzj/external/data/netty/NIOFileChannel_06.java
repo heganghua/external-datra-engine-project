a93de18f61486e29b101932b1ee3e7b645c0e024
heganghua <heganghua1113@163.com>
2021-10-13 21:19:39 +0800

    ServerSocketChannel

---
+++ external-data-engine/src/main/java/com/jxzj/external/data/netty/NIOFileChannel_06.java
@@ -0,0 +1,31 @@
+package com.jxzj.external.data.netty;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
+
+/**
+ * 
+ * @author heganghua
+ * @date 2021/10/13
+ */
+public class NIOFileChannel_06 {
+
+    public static void main(String[] args) throws IOException {
+
+        RandomAccessFile randomAccessFile = new RandomAccessFile("D://file01.txt", "rw");
+
+        FileChannel channel = randomAccessFile.getChannel();
+
+        MappedByteBuffer map = channel.map(MapMode.READ_WRITE, 0, 52);
+
+        map.put(0, (byte)'b');
+        map.put(51, (byte)'G');
+
+        randomAccessFile.close();
+
+    }
+
+}
