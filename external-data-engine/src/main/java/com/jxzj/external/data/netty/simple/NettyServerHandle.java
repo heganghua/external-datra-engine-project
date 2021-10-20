fd2fc80b5761182f66abe963f1460afcc2cd2690
heganghua <heganghua1113@163.com>
2021-10-20 22:45:07 +0800

    NettyDemo

---
+++ external-data-engine/src/main/java/com/jxzj/external/data/netty/simple/NettyServerHandle.java
@@ -0,0 +1,7 @@
+package com.jxzj.external.data.netty.simple;
+
+import com.alibaba.dubbo.remoting.transport.ChannelHandlerAdapter;
+
+public class NettyServerHandle extends ChannelHandlerAdapter {
+
+}
