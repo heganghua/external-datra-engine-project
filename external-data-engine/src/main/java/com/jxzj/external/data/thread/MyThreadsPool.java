6bd28a7b5323ee4b564f61c7717855713324648c
heganghua <heganghua1113@163.com>
2022-01-25 09:40:52 +0800

    信号量

--- external-data-engine/src/main/java/com/jxzj/external/data/thread/MyThreadsPool.java
+++ external-data-engine/src/main/java/com/jxzj/external/data/thread/MyThreadsPool.java
@@ -3,7 +3,6 @@ package com.jxzj.external.data.thread;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.stream.IntStream;
 
 public class MyThreadsPool {
@@ -21,19 +20,33 @@ public class MyThreadsPool {
         });
     }
 
+    /**
+     * 1、核心线程大小 corepoolsize<br>
+     * 2、最大线程大小maxipoolsize<br>
+     * 3、存活时间， alivetime <br>
+     * 4、时间单位，unit <br>
+     * 5、同步阻塞工作队列， blockingArrayQueue <br>
+     * 6、线程工厂 ThreadFactor<br>
+     * 7、拒绝策略 rejectedExecutionHeadle
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
 
-        MyThreadsPool mp = new MyThreadsPool(new LinkedBlockingQueue<>(10), 3);
+        System.out.println(1 << 29);
+        System.out.println(Integer.SIZE);
 
-        IntStream.rangeClosed(1, 5).forEach((i) -> {
-            try {
-                mp.execute(() -> {
-                    System.out.println(Thread.currentThread().getName() + " 公众号：yes的练级攻略");
-                });
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+        // MyThreadsPool mp = new MyThreadsPool(new LinkedBlockingQueue<>(10), 3);
+        //
+        // IntStream.rangeClosed(1, 5).forEach((i) -> {
+        // try {
+        // mp.execute(() -> {
+        // System.out.println(Thread.currentThread().getName() + " 公众号：yes的练级攻略");
+        // });
+        // } catch (InterruptedException e) {
+        // e.printStackTrace();
+        // }
+        // });
 
     }
 
---
+++ external-data-engine/src/main/java/com/jxzj/external/data/thread/SemaphoreDemo.java
@@ -0,0 +1,46 @@
+package com.jxzj.external.data.thread;
+
+import java.util.Random;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class SemaphoreDemo implements Runnable {
+
+    private static final int COUNT = 20;
+
+    public static void main(String[] args) {
+
+        ThreadPoolExecutor executor =
+            new ThreadPoolExecutor(COUNT, COUNT, 0, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+
+        // 指定最多只有五个线程同时执行
+        Semaphore semaphore = new Semaphore(5);
+        Random random = new Random();
+        for (int i = 0; i < COUNT; i++) {
+            final int no = i;
+            executor.execute(() -> {
+                try {
+                    // 获得许可
+                    semaphore.acquire();
+
+                    System.out.println(no + ": 号车可通行");
+                    Thread.sleep(10 * 1000);
+                    // 释放许可
+                    semaphore.release();
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        executor.shutdown();
+    }
+
+    @Override
+    public void run() {
+        }
+    }
+
+}
